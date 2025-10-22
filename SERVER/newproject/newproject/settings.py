9c61612293c84c276ab7d5e6d4fef316c3fb4aa9
Sharon-software <sharonshadracks23@gmail.com>
2025-10-22 15:03:54 +0200

    Ready for Render deployment

--- SERVER/newproject/newproject/settings.py
+++ SERVER/newproject/newproject/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from corsheaders.defaults import default_headers
+import os
+import dj_database_url
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9zzc)!qgjrkn0p84!!jr2v_l56gcvqjq35mkc$95-u37^v-s!0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -82,10 +85,9 @@ WSGI_APPLICATION = 'newproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+      'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')  # Render PostgreSQL URL
+    )
 }
 
 
@@ -124,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -136,8 +140,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    
+]+os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
 
-]
 CORS_ALLOW_HEADERS = list(default_headers) + [
     "Authorization",
 ]
@@ -156,7 +161,13 @@ SIMPLE_JWT = {
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://everythingbeauty.onrender.com",
 ]
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-for-local')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
+    
 CORS_ALLOW_CREDENTIALS = True 
 CSRF_COOKIE_NAME = "csrftoken"
 CSRF_COOKIE_HTTPONLY = False

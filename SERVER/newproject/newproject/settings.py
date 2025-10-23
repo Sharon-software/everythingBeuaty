017a026da48e5d5c2d5594de9b442ad2b6c3e098
Sharon-software <sharonshadracks23@gmail.com>
2025-10-23 08:07:59 +0200

    remove hardcoded secret key

--- SERVER/newproject/newproject/settings.py
+++ SERVER/newproject/newproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9zzc)!qgjrkn0p84!!jr2v_l56gcvqjq35mkc$95-u37^v-s!0'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

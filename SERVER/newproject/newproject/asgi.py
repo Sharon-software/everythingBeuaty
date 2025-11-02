8756ea901c64ced18615554666c92c9781ef5b1c
Sharon-software <sharonshadracks23@gmail.com>
2025-11-02 21:22:19 +0200

    fix asgi.py

--- SERVER/newproject/newproject/asgi.py
+++ SERVER/newproject/newproject/asgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
 settings_module = 'api.Deployment_settings' if "RENDER_EXTERNAL_HOSTNAME" in os.environ else 'newproject.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '.settings_module')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_module')
 
 application = get_asgi_application()
 
